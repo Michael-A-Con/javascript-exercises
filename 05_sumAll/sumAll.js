294a96867de835b98deb025bc192dc00a0da03e2
Michael-A-Con <michaelandrescon@gmail.com>
2024-01-24 08:22:18 -0600

    finished exercises 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
 
+    if (typeof min !== "number" || typeof max !== "number" ) {
+        return "ERROR";
+    }
+
+    if (min < 0 || max < 0) {
+        return "ERROR";
+    }
+
+    if (min < max) {
+        return ((max-min)+1) * (min + max)/2;
+    } else if (min > max) {
+        return ((min-max)+1) * (max + min)/2;
+    }
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (!Number.isInteger(year)) {
+        return false;
+    }
+
+    if (year % 4 === 0) {
+        if (year % 100 === 0 && year % 400 === 0) {
+            return true;
+        } else if (year % 100 === 0 && year % 400 !== 0) {
+            return false;
+        }
+        return true;
+    }
+
+
+    return false;
 };
 
 // Do not edit below this line
