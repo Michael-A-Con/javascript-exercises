7132e8ee15a06d3901d0d585ab5c43af296c1ceb
Michael-A-Con <michaelandrescon@gmail.com>
2024-02-13 10:11:31 -0600

    finished all challenges

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge > currentAge ? oldest : current;
+    });
+};
 
+const getAge = function (born, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+
+    return death - born;
 };
 
 // Do not edit below this line
