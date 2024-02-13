e6938d9ee0eed71a81b0ca5c6c576521ec930239
Michael-A-Con <michaelandrescon@gmail.com>
2024-02-12 23:09:31 -0600

    Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    const processedString = input.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let array = processedString.split('');
+    array.reverse()
 
+    console.log(array.toString());
+
+    if (array.join('') === processedString) return true;
+
+    return false;
 };
 
 // Do not edit below this line
